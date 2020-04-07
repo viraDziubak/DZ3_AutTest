e3aeb48ae78c13df9493c149f8977a2999252502
viraDziubak <viradziubak806@gmail.com>
2020-04-07 17:24:56 +0300

    Initial commit

---
+++ DZ3_AutTest/Tests/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DZ3_AutTest.Tests
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        static void Main (string[] args)
+        {
+        }
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("hbh");
+        } 
+    }
+
+   
+    
+}
